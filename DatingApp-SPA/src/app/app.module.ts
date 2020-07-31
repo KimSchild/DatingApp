9540e2944c4991ffd8932a458030db1500911c70
KimSchild <kim.ida@web.de>
2020-07-31 13:15:26 +0200

    End of section 7

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
 import {ErrorInterceptorProvider} from './_services/error.interceptor';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import {RouterModule} from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
@@ -12,7 +14,10 @@ import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-
+import { MemberListComponent } from './member-list/member-list.component';
+import { ListsComponent } from './lists/lists.component';
+import { MessagesComponent } from './messages/messages.component';
+import {appRoutes} from './roots';
 
 @NgModule({
    declarations: [
@@ -21,7 +26,10 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
       HomeComponent,
       RegisterComponent,
       HomeComponent,
-      RegisterComponent
+      RegisterComponent,
+      MemberListComponent,
+      ListsComponent,
+      MessagesComponent
    ],
    imports: [
       BrowserModule,
@@ -30,7 +38,9 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
       ReactiveFormsModule,
       BsDropdownModule.forRoot(),
       BrowserAnimationsModule,
-      FontAwesomeModule
+      FontAwesomeModule,
+      RouterModule.forRoot(appRoutes),
+      CommonModule
    ],
    providers: [
       AuthService,
